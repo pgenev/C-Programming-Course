c30709d21bf5f516d7297f6e6e7e50b35bf96aea
pgenev <philip_genev@abv.bg>
2016-11-07 16:55:01 +0200

    not yet done

--- Unit2/task6.c
+++ Unit2/task6.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#include <stdint.h>
 
-uint32_t BCDadd(uint32_t a,uint32_t b)
-{
-    uint32_t  t1, t2;    // unsigned 32-bit intermediate values
+void bcd(int number){
+	int numVal = number;
+	int result = 0;
+	while(number > 0){
+		result <<= 4;
+		result |= number % 10;
+		number /= 10;	
+	}
+	printf("\nBCD of %d decimal number: result = 0x%x\n\n",numVal, result);
+	printf("\nReal value of number = %d\n\n", numVal);
+	printf("\nReal value of result = %d\n\n", result);
 
-    t1 = a + 0x06666666;
-    t2 = t1 ^ b;                   // sum without carry propagation
-    t1 = t1 + b;                   // provisional sum
-    t2 = t1 ^ t2;                  // all the binary carry bits
-    t2 = ~t2 & 0x11111110;         // just the BCD carry bits
-    t2 = (t2 >> 2) | (t2 >> 3);    // correction
-    return t1 - t2;                // corrected BCD sum
 }
 
 void main(){
 
-	printf("%d\n", BCDadd(10, 12));
+	int number = 123; // 123(decimal)
+	//int number2 = 514; // 514 (decimal)
+	bcd(123);
+	//bcd(514);
 
 }
