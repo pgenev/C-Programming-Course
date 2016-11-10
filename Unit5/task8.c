4686940a1de1feec67f8b96dd11598ae5e179016
pgenev <philip_genev@abv.bg>
2016-11-10 13:49:57 +0200

    done

---
+++ Unit5/task8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TRUE 0
+#define FALSE 1
+
+void main(){
+
+	char sentence[] = "I am a sentence    and I    want to be read   !   ";
+	printf("\nFull string: %s\n\n", sentence);
+	int strLength = strlen(sentence);
+	char *ptr = sentence;
+	int add_quote = TRUE;
+	
+	while('\0' != *ptr){
+		if(TRUE == add_quote && ' ' != *ptr){
+			printf("'");
+			add_quote = FALSE;
+		}
+		if(' ' == *ptr && ' ' != *(ptr+1)){
+			add_quote = TRUE;
+			ptr++;
+			printf("'");
+			printf("\n");
+			continue;
+		}
+		if(' ' == *ptr){
+			ptr++;
+			continue;
+		}
+		printf("%c", *(ptr++));
+		if('\0' == *ptr){
+			printf("'");
+			printf("\n");
+			break;
+		}	
+	}
+
+	return;
+}
