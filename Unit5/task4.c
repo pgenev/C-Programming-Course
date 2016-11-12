ba97fc6174aa2e8e043554fe8447fad3c8b8f6d9
pgenev <philip_genev@abv.bg>
2016-11-12 18:09:46 +0200

    question about free()

--- Unit5/task4.c
+++ Unit5/task4.c
@@ -6,11 +6,11 @@
 
 void initializeArray(int **arr, int rows, int cols)
 {	
-	int element = 0;
+	int element = 9;
 
-	for(int i=0; i <= rows-1 ; ++i)
+	for(int i=0; i < rows ; ++i)
 	{
-    	for(int j=0; j <= cols-1 ; ++j)
+    	for(int j=0; j < cols ; ++j)
      	{
         	arr[i][j] = ++element;
      	}
@@ -19,7 +19,7 @@ void initializeArray(int **arr, int rows, int cols)
 
 int **malloc2d(int row, int col)
 {
-	int i=0;
+	int i = 0;
 	int **arr = NULL;
 
 	arr = malloc(row * sizeof(int *));
@@ -43,9 +43,9 @@ int **malloc2d(int row, int col)
 
 int **transponseMatrix(int **arr,int **arr2, int rows, int cols)
 {	
-	for(int i=0; i <= cols-1; ++i)
+	for(int i=0; i < cols ; ++i)
 	{
-    	for(int j=0; j <= rows-1; ++j)
+    	for(int j=0; j < rows ; ++j)
      	{
      		arr2[i][j] = arr[j][i];
      	}
@@ -61,11 +61,14 @@ void printMatrix(int **arr, int transposed, int rows, int cols)
 		printf("\nMatrix before transponse\n\n");
 	} else if(TRUE == transposed)
 	{
+		int tmp = rows;
+		rows = cols;
+		cols = tmp;
 		printf("\nMatrix after transponse\n\n");
 	}
-	for(int i=0; i <= rows-1; ++i)
+	for(int i=0; i < rows ; ++i)
 	{
-    	for(int j=0; j <= cols-1; ++j)
+    	for(int j=0; j < cols ; ++j)
      	{
         	printf("%d ", arr[i][j]);
      	}
@@ -73,25 +76,39 @@ void printMatrix(int **arr, int transposed, int rows, int cols)
 	}
 }
 
+void free2d(int **arr, int rows)
+{
+	for (int i = 0; i < rows; ++i) 
+	{ 
+      free((void *)arr[i]);
+      arr[i] = NULL;
+  	}
+  	free((void **)arr);
+  	arr = NULL;
+}
+
 
 void main()
 {
-	int rows = 3;
-	int cols = 3;
+	int rows = 9;
+	int cols = 10;
 
 	int **arrPtr = malloc2d(rows, cols);
-	int **arrPtr2 = malloc2d(cols, rows);
+	int **arrTrans = malloc2d(cols, rows);
 
 	initializeArray(arrPtr, rows, cols);
 
 	printMatrix(arrPtr, FALSE, rows, cols);
-	int **transPtr = transponseMatrix(arrPtr, arrPtr2, rows, cols);
-	
+	int **transPtr = transponseMatrix(arrPtr, arrTrans, rows, cols);
 	printMatrix(transPtr, TRUE, rows, cols);
 
-	
-	
-	
+	free2d(arrPtr, rows);
+	free2d(arrTrans, cols);
+
+	if(NULL != arrPtr)
+	{
+		printf("\nfree the memory!\n\n");
+	}
 
 	return;
 }
